f128a5eba6ac2bb8370ccc422f695652d2465691
csslick <digitone@naver.com>
2021-10-01 15:39:15 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// main.js
+$(function(){
+
+  // 색상 버튼을 클릭하면 이미지 변경
+  var imgUrl = [
+    'images/black.webp',
+    'images/green.webp',
+    'images/gray.webp',
+  ]
+
+  // 해당 버튼 클릭시 imgUrl 배열번호로 img태그 src를 변경
+  
+
+
+});
