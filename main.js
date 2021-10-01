abf272a5733d94e660405f2613df2c1e90f235da
csslick <digitone@naver.com>
2021-10-01 16:17:57 +0900

    옵션 변경시 가격연동

--- main.js
+++ main.js
@@ -9,7 +9,35 @@ $(function(){
   ]
 
   // 해당 버튼 클릭시 imgUrl 배열번호로 img태그 src를 변경
+  $('.colorOption .color').on('click', function(){
+    var i = $(this).index();
+    console.log(i)
+    $('#product-detail > figure > img').attr('src', imgUrl[i]);
+  })
+
+  // 메모리
+  const price = 1998700; // 기본가격
+
+  $('#memory').on('change', function(){
+    var value = $(this).val();    
+    $('.memoryOption .memory').text(value);
+
+    var memoryPrice = 100000;
+    if(value == 512) {
+      var totalPrice = (price + memoryPrice).toLocaleString();
+      $('.total-price').text(totalPrice);
+    } else if(value == 256) {
+      $('.total-price').text(price);
+    }
+
+  })
   
+  // 수량
+  $('.numberOption .count').on('change', function(){
+    var value = $(this).val();
+    var totalPrice = (price * value).toLocaleString();
+    $('.total-price').text(totalPrice);
+  })
 
 
-});
+}); // end $
